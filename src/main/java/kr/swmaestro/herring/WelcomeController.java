1ae8475a6c2948bb93e5ddc4cb3b3c601f0a227d
Chiwan Park <chiwanpark91@gmail.com>
2013-04-16 09:06:38 +0900

    create maven project

---
+++ src/main/java/kr/swmaestro/herring/WelcomeController.java
@@ -0,0 +1,22 @@
+package kr.swmaestro.herring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 메인 페이지를 출력하는 클래스.
+ *
+ * @author Chiwan Park
+ * @since 0.1
+ */
+
+@Controller
+@RequestMapping("/")
+public class WelcomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String showMain(ModelMap model) {
+        return "main";
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/main.jsp
@@ -0,0 +1,11 @@
+<!doctype html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Herring</title>
+</head>
+<body>
+<h1>Herring</h1>
+</body>
+</html>
